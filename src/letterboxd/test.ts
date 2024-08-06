35f85e9e8b05d8f3e45dc4062a073cfb5f6c8ab1
Luis Finke <luisfinke@gmail.com>
2024-08-05 23:32:34 -0400

    update username

--- src/letterboxd/test.ts
+++ src/letterboxd/test.ts
@@ -3,7 +3,7 @@ import * as letterboxd from './index';
 
 (async () => {
 	try {
-		const result = await letterboxd.fetchUserFollowingFeed('fathersteak');
+		const result = await letterboxd.fetchUserFollowingFeed('luisfinke');
 		console.log(JSON.stringify(result, null, '\t'));
 	} catch(error) {
 		console.error(error);
