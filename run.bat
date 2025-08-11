0bb34ef4c75e81ed769bf0d51dee1b3b81eef122
Luis Finke <luisfinke@gmail.com>
2025-08-11 18:46:58 -0400

    set NODE_ENV=production in run scripts

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+set NODE_ENV=production
 cd %~dp0% || exit /b
 call npm install || exit /b
 call npm start -- --config=config/config.json
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export NODE_ENV=production
 cd "$(dirname "$(realpath "$0")")" || exit $?
 npm install || exit $?
 npm start -- --config=config/config.json || exit $?
