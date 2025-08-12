b4309f581656136953398b16f66c00f81aadcc76
Luis Finke <luisfinke@gmail.com>
2025-08-11 20:18:04 -0400

    use build command in run scripts

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 set NODE_ENV=production
 cd %~dp0% || exit /b
-call npm install || exit /b
+call npm run build || exit /b
 call npm start -- --config=config/config.json
 pause
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export NODE_ENV=production
 cd "$(dirname "$(realpath "$0")")" || exit $?
-npm install || exit $?
+npm run build || exit $?
 npm start -- --config=config/config.json || exit $?
