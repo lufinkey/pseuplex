dc3a7a0dd418505edbd1ff36f39ef19c3e9012b6
Luis Finke <luisfinke@gmail.com>
2025-08-11 20:06:03 -0400

    include branch in app version string

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -6,7 +6,8 @@ import packageJson from '../../package.json';
 export type AppVersion = {
 	version: string;
 	tag: string | undefined;
-	commitHash: string | undefined;
+	branch: string | undefined;
+	commit: string | undefined;
 	dirty: boolean | undefined;
 };
 
@@ -17,51 +18,70 @@ export const getAppVersion = async (): Promise<AppVersion> => {
 	let gitTag: string | undefined;
 	let gitCommitHash: string | undefined;
 	let gitDirty: boolean | undefined;
+	let gitBranch: string | undefined;
 	const isGitRepo = await new Promise((resolve, reject) => fs.exists(`${modulePath}/.git`, resolve));
 	if(isGitRepo) {
 		// parse git tag
-		try {
-			const output = await executeAndGetOutputAsync('git', ['tag', '--points-at', 'HEAD']);
-			const outputString = output.toString('utf8');
-			const firstLine = getFirstLineOfString(outputString);
-			if(firstLine) {
-				gitTag = firstLine;
-			}
-		} catch(error) {
-			console.error(`Error calling git tag command:`);
-			console.error(error);
-		}
-		// parse git commit hash
-		try {
-			const output = await executeAndGetOutputAsync('git', ['rev-parse', 'HEAD']);
-			const outputString = output.toString('utf8');
-			const firstLine = getFirstLineOfString(outputString);
-			if(firstLine) {
-				gitCommitHash = firstLine;
-			}
-		} catch(error) {
-			console.error(`Error calling git rev-parse command:`);
-			console.error(error);
-		}
-		// check if git has any revisions
-		try {
-			const output = await executeAndGetOutputAsync('git', ['status', '-s']);
-			const outputString = output.toString('utf8').trim();
-			if(outputString) {
-				gitDirty = true;
-			} else {
-				gitDirty = false;
-			}
-		} catch(error) {
-			console.error(`Error calling git rev-parse command:`);
-			console.error(error);
-		}
+		[gitTag, gitCommitHash, gitDirty, gitBranch] = await Promise.all([
+			(async () => {
+				try {
+					const output = await executeAndGetOutputAsync('git', ['tag', '--points-at', 'HEAD']);
+					const outputString = output.toString('utf8');
+					const firstLine = getFirstLineOfString(outputString);
+					if(firstLine) {
+						return firstLine;
+					}
+				} catch(error) {
+					console.error(`Error calling git tag command:`);
+					console.error(error);
+				}
+			})(),
+			(async () => {
+				// parse git commit hash
+				try {
+					const output = await executeAndGetOutputAsync('git', ['rev-parse', 'HEAD']);
+					const outputString = output.toString('utf8');
+					const firstLine = getFirstLineOfString(outputString);
+					if(firstLine) {
+						return firstLine;
+					}
+				} catch(error) {
+					console.error(`Error calling git rev-parse command:`);
+					console.error(error);
+				}
+			})(),
+			(async () => {
+				// check if git has any revisions
+				try {
+					const output = await executeAndGetOutputAsync('git', ['status', '-s']);
+					const outputString = output.toString('utf8').trim();
+					return outputString ? true : false;
+				} catch(error) {
+					console.error(`Error calling git rev-parse command:`);
+					console.error(error);
+				}
+			})(),
+			(async () => {
+				try {
+					const output = await executeAndGetOutputAsync('git', ['branch', '--show-current']);
+					const outputString = output.toString('utf8');
+					const firstLine = getFirstLineOfString(outputString);
+					if(firstLine) {
+						return firstLine;
+					}
+				} catch(error) {
+					console.error(`Error calling git tag command:`);
+					console.error(error);
+				}
+			})(),
+		]);
 	}
 
 	return {
 		version: packageJson.version,
 		tag: gitTag,
-		commitHash: gitCommitHash,
+		commit: gitCommitHash,
+		branch: gitBranch,
 		dirty: gitDirty,
 	};
 };
@@ -71,16 +91,23 @@ export const getAppVersionString = async (): Promise<string> => {
 	if(appVersion.tag) {
 		return `${appVersion.tag}` + (appVersion.dirty ? ' (dirty)' : '');
 	} else if(appVersion.version) {
-		if(appVersion.commitHash) {
-			return `v${appVersion.version}`
-				+ (appVersion.commitHash
-					? ` (rev ${appVersion.commitHash}${appVersion.dirty ? ', dirty' : ''})`
-					: '');
+		const parenthesParts: string[] = [];
+		if(appVersion.branch) {
+			parenthesParts.push(`branch ${appVersion.branch}`);
+		}
+		if(appVersion.commit) {
+			parenthesParts.push(`commit ${appVersion.commit}`);
+		}
+		if(appVersion.dirty) {
+			parenthesParts.push('dirty');
+		}
+		if(parenthesParts.length > 0) {
+			return `v${appVersion.version} (${parenthesParts.join(", ")})`;
 		} else {
 			return `v${appVersion.version}`;
 		}
-	} else if(appVersion.commitHash) {
-		return `rev ${appVersion.commitHash}` + (appVersion.dirty ? ' (dirty)' : '');
+	} else if(appVersion.commit) {
+		return `rev ${appVersion.commit}` + (appVersion.dirty ? ' (dirty)' : '');
 	} else {
 		return "v?.?.?";
 	}
