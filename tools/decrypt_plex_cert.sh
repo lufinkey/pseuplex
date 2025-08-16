a0e3fa1fc8376fb1dabe710c72d8c23d78ff75d7
Luis Finke <luisfinke@gmail.com>
2025-08-16 17:56:23 -0400

    make keys folder if missing, output to stderr

--- tools/decrypt_plex_cert.sh
+++ tools/decrypt_plex_cert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# validate args
+# validate that all args are flags
 for arg in "$@"; do
 	if [[ ! $arg == -* ]]; then
-		echo "Invalid arg $arg"
+		>&2 echo "Invalid arg $arg"
 	fi
 done
 
@@ -13,13 +13,16 @@ plexutils_path="$base_path/tools/plex_utils.sh"
 privatekey_path="$base_path/keys/plex_privatekey.pem"
 certchain_path="$base_path/keys/plex_certchain.pem"
 
+# create keys folder if it doesnt exist
+mkdir -p "$base_path/keys" || exit $?
+
 # extract ssl keys
-echo "Extracting plex private key"
+>&2 echo "Extracting plex private key"
 "$plexutils_path" "$@" ssl-cert output-privatekey "$privatekey_path" || exit $?
-echo "Updating privatekey permissions"
+>&2 echo "Updating privatekey permissions"
 chmod 600 "$privatekey_path" || exit $?
 
-echo "Extracting plex certificate chain"
+>&2 echo "Extracting plex certificate chain"
 "$plexutils_path" "$@" ssl-cert output-certchain "$certchain_path" || exit $?
-echo "Updating certchain permissions"
+>&2 echo "Updating certchain permissions"
 chmod 644 "$certchain_path" || exit $?
