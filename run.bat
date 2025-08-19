5ae60af6ababa96b38d32288a2290d6298a737ef
Luis Finke <luisfinke@gmail.com>
2025-08-19 00:24:23 -0400

    fix cd into nonexistant path, add --log-watched-paths to batch script

--- run.bat
+++ run.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
 (
-	cd %~dp0% || goto :exit
+	cd "%~dp0" || goto :exit
 	call npm install || goto :exit
 	call npm run build || goto :exit
 	set NODE_ENV=production
-	call npm start -- --config=config/config.json || goto :exit
+	call npm start -- --config=config/config.json --log-watched-paths || goto :exit
 )
 endlocal
 
