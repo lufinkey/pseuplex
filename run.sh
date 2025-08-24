f5a86de2f489903fc1fbc20f4f717bfe35345fcd
Luis Finke <luisfinke@gmail.com>
2025-08-24 16:01:42 -0400

    remove --verbose-ws-traffic, add --log-watchedpaths to run.sh

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ npm run build || exit $?
 
 # run the app
 export NODE_ENV=production
-npm start -- --config=config/config.json --verbose-ws-traffic || exit $?
+npm start -- --config=config/config.json --log-watched-paths || exit $?
