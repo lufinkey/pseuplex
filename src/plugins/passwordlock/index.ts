0a6bfbe8d3f91bff80e058555267297a068f9ad7
Luis Finke <luisfinke@gmail.com>
2025-08-25 23:03:05 -0400

    handle section all items from library base path

--- src/plugins/passwordlock/index.ts
+++ src/plugins/passwordlock/index.ts
@@ -224,6 +224,14 @@ export default (class PasswordLockPlugin implements PasswordLockPluginDef, Pseup
 			}),
 		]);
 
+		unauthRouter.get(`/library/sections/${this.section.id}/all`, [
+			this.app.middlewares.plexAPIRequestHandler(async (req: IncomingPlexAPIRequest, res) => {
+				const context = this.app.contextForRequest(req);
+				const plexParams = plexTypes.parsePlexSectionAllItemsPageParams(req);
+				return await this.section.getAllItemsPage(plexParams, context);
+			}),
+		]);
+
 		unauthRouter.get(this.section.hubsPath, [
 			this.app.middlewares.plexAPIRequestHandler(async (req: IncomingPlexAPIRequest, res) => {
 				const context = this.app.contextForRequest(req);
