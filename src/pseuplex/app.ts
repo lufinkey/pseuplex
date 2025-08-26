05059f1afe551f2fbd01e0091f226eab36debfac
Luis Finke <luisfinke@gmail.com>
2025-08-25 23:37:58 -0400

    i guess we include event subscribers

--- src/pseuplex/app.ts
+++ src/pseuplex/app.ts
@@ -2414,7 +2414,7 @@ export class PseuplexApp {
 				}
 			}
 		}
-		/*if(eventSubscribers) {
+		if(eventSubscribers) {
 			for(const subscriber of eventSubscribers) {
 				if(!subscriber.proxyResponse) {
 					// request hasn't received a response from the server yet, so we shouldn't send any notifications
@@ -2426,7 +2426,7 @@ export class PseuplexApp {
 					response: subscriber.response
 				});
 			}
-		}*/
+		}
 		return senders;
 	}
 
