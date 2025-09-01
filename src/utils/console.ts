388379b3f846101db2551c8b047d5a9e8fbff12d
Luis Finke <luisfinke@gmail.com>
2025-09-01 15:57:24 -0400

    iso string is more descriptive

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -50,7 +50,7 @@ export const includeTimestampsForAllLogs = () => {
 	includedTimestamps = true;
 
 	function insertTimestampArg(args: any[]) {
-		args.splice(0, 0, `[${(new Date()).toLocaleString()}]`);
+		args.splice(0, 0, `[${(new Date()).toISOString()}]`);
 	}
 
 	const innerError = console.error;
