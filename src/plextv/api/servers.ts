5b26ae6c19192bc98b070baa74f7b0481178a1ce
Luis Finke <luisfinke@gmail.com>
2025-11-01 17:16:22 -0400

    add access tokens endpoint to client

--- src/plextv/api/servers.ts
+++ src/plextv/api/servers.ts
@@ -1,6 +1,6 @@
 import { PlexAuthContext } from '../../plex/types';
 import { PlexTVAPIRequestOptions, plexTVFetch } from './core';
-import { PlexTVSharedServersPage } from '../types';
+import { PlexTVAccessTokensPage, PlexTVSharedServersPage } from '../types';
 
 export const getSharedServers = async (args: {
 	clientIdentifier: string,
@@ -11,3 +11,14 @@ export const getSharedServers = async (args: {
 		endpoint: `api/servers/${args.clientIdentifier}/shared_servers`,
 	});
 };
+
+export const getAccessTokens = async (options: PlexTVAPIRequestOptions): Promise<PlexTVAccessTokensPage> => {
+	return await plexTVFetch<PlexTVAccessTokensPage>({
+		...options,
+		method: 'GET',
+		endpoint: `api/v2/server/access_tokens`,
+		headers: {
+			'accept': 'application/json'
+		}
+	});
+}
